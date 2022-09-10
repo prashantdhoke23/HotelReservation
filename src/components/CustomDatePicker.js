35eb4787950c37a385eab94aca71832b409dd338
Priyanka Bharade <priyankabharade7apr@gmail.com>
2022-09-10 22:35:50 +0530

    registration form

---
+++ src/components/CustomDatePicker.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import dayjs from "dayjs";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import moment from "moment";
+import Box from "@mui/material/Box";
+
+const CustomDatePicker = () => {
+  const [value, setValue] = React.useState(dayjs(moment().format()));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "70ch" },
+        marginTop: 1,
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Stack spacing={4}>
+          <DesktopDatePicker
+            label="Date desktop"
+            inputFormat="MM/DD/YYYY"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
+    </Box>
+  );
+};
+export default CustomDatePicker;
---
+++ src/components/Sample.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+
+export default function Sample() {
+  const [roomDetail, setroomDetails] = useState([]);
+  useEffect(() => {
+    setroomDetails([
+      { id: 1, fieldName: "Name", type: "input", addl_inputs: "max 50 chars" },
+      {
+        id: 2,
+        fieldName: "email Id",
+        type: "input",
+        addl_inputs: "validate format",
+      },
+      {
+        id: 3,
+        fieldName: "Room Type",
+        type: "dropDown",
+        values: ["Standard", "Private", "Dorm"],
+      },
+      {
+        id: 4,
+        fieldName: "Number of Guests",
+        type: "Number",
+        addl_inputs: "minimum 2",
+      },
+      { id: 5, fieldName: "Check in Date", type: "Calendar" },
+    ]);
+    // fetch('https://apis-react-training.herokuapp.com/getFormDetails')
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch(erroe => console.log("Catched Error is " + erroe));
+    // fetch("https://apis-react-training.herokuapp.com/getFormDetails")
+    //   .then((response) => {
+    //     if (response.status === 200 || response.ok) {
+    //       return response.json();
+    //     } else {
+    //       throw new Error(`HTTP error status: ${response.status}`);
+    //     }
+    //   })
+    //   .then((json) => {
+    //     console.log(json);
+    //   });
+  }, []);
+  return (
+    <div className="container">
+      <div className="card mt-5">
+        <form className="card-body">
+          <div className="form-group mb-3">
+            <label className="mb-2">
+              <strong>Name</strong>
+            </label>
+            <input type="text" className="form-control" />
+            <small className="text-danger">Error section</small>
+          </div>
+          <div className="form-group mb-3">
+            <label className="mb-2">
+              <strong>Email</strong>
+            </label>
+            <input type="email" className="form-control" />
+          </div>
+          <div className="form-group mb-3">
+            <label className="mb-2">
+              <strong>Password</strong>
+            </label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="d-grid mt-3">
+            <button type="submit" className="btn btn-block btn-primary">
+              Submit
+            </button>
+          </div>
+          {roomDetail.map((item, index) => (
+          <textarea>{item.fieldName}</textarea>
+          ))}
+          
+        </form>
+      </div>
+    </div>
+  );
+}
