51bfa14a64f65d95154461bacb1d5e8f9f623139
Priyanka Bharade <priyankabharade7apr@gmail.com>
2022-09-12 10:16:45 +0530

    add background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import './App.css';
-import BookingForm from './bookingForm/BookingForm';
-import './components/image/back-img.jpg';
+import "./App.css";
+import BookingForm from "./bookingForm/BookingForm";
+import "./components/image/back-img.jpg";
+import image from "./components/image/back-img.jpg";
 
-const myStyle={
-//   backgroundImage: 
-// "url('https://media.geeksforgeeks.org/wp-content/uploads/rk.png')",
-//   height:'100vh',
-//   marginTop:'-50px',
-//   fontSize:'20px',
-//   backgroundSize: 'cover',
-//   backgroundRepeat: 'no-repeat',
+const myStyle = {
+  //   backgroundImage:
+  // "url('https://media.geeksforgeeks.org/wp-content/uploads/rk.png')",
+  //   height:'100vh',
+  //   marginTop:'-50px',
+  //   fontSize:'20px',
+  //   backgroundSize: 'cover',
+  //   backgroundRepeat: 'no-repeat',
+};
 
-}
-
-const imageBack=new URL("./image/back-img.jpg",import.meta.url)
 function App() {
   return (
-    <div className="App">
-      
-      <div className='image'>
-      <img src='imageBack' />
-      </div>
-       <BookingForm/>
+    <div className="App" style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat" }}>
+      <BookingForm />
     </div>
   );
 }
